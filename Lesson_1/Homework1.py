04d8d6da1f07c0f84c38a77ee96ddbf10d14f629
hislevin <slevin.snb@gmail.com>
2023-06-29 00:05:52 -0700

    added the first homework

---
+++ Lesson_1/Homework1.py
@@ -0,0 +1,11 @@
+# reversed() in Python
+
+# numbers = [1, 2, 3, 4, 5]
+# for x in reversed(numbers):
+#     print(x)
+
+# same task 
+
+numbers = [1, 2, 3, 4, 5]
+for x in numbers[::-1]:
+    print(x)
